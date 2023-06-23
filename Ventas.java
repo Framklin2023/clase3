89330a12e28b7ff2599c8fb39f3cc64c10cc6c90
Framklin2023 <137226922+Framklin2023@users.noreply.github.com>
2023-06-22 20:00:46 -0500

    listos sin arrays

--- Ventas.java
+++ Ventas.java
@@ -1,6 +1,10 @@
 public class Ventas {
     public static void main(String[] args) {
-        System.out.println("Funciona");
+
+        Afiliado maria = new Afiliado("Tolosa", "María", "Lima", 1, 250);
+        Cliente john = new Cliente("Rivas", "Héctor", "Lima", "9999");
+        System.out.println(maria.getApellido());
+        System.out.println(john.getTelefono());
     }
 }
 
@@ -39,4 +43,66 @@ abstract class Person {
         this.direccion = direccion;
     }
 
+}
+
+class Afiliado extends Person implements Pedido {
+    private long id;
+    private float monto;
+
+    public Afiliado(String apellido, String nombre, String direccion, long id, float monto) {
+        super(apellido, nombre, direccion);
+        this.id = id;
+        this.monto = monto;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    @Override
+    public String orden() {
+        String msg = " El afiliado " + getId() + " debe vender de : " + getMonto() + " Soles";
+        return msg;
+    }
+
+}
+
+class Cliente extends Person implements Pedido {
+    private String telefono;
+
+    public Cliente(String apellido, String nombre, String direccion, String telefono) {
+        super(apellido, nombre, direccion);
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String orden() {
+        String msg = " El cliente " + getApellido() + " " + getNombre() + " telefono: " + getTelefono();
+        return msg;
+    }
+
+}
+
+interface Pedido {
+    public String orden();
 }
