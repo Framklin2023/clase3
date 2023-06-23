b6c0ef113b031ed4a19c2aee03a25e532614a5a2
Framklin2023 <137226922+Framklin2023@users.noreply.github.com>
2023-06-22 20:59:00 -0500

    tercer commit

--- Ventas.java
+++ Ventas.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+
 public class Ventas {
     public static void main(String[] args) {
 
-        Afiliado maria = new Afiliado("Tolosa", "María", "Lima", 1, 250);
-        Cliente john = new Cliente("Rivas", "Héctor", "Lima", "9999");
-        System.out.println(maria.getApellido());
-        System.out.println(john.getTelefono());
+        ArrayList<Cliente> clts = new ArrayList<Cliente>();
+        System.out.println("-----------------------------------------------------------------------");
+        clts.add(new Cliente("Rivas", "Héctor", "Lima", "9999"));
+        clts.add(new Cliente("Saldaña", "Camila", "Ayacucho", "7777999"));
+        clts.add(new Cliente("Pulse", "Yennifer", "Tarapoto", "9995559"));
+        clts.add(new Cliente("Hualinus", "Jenny", "Piura", "7079999"));
+        clts.forEach(c -> System.out.println(c.toString()));
+        System.out.println("-----------------------------------------------------------------------");
+
+        Afiliado[] afls = new Afiliado[3];
+        afls[0] = new Afiliado("Tolosa", "María", "Lima", 1, 250);
+        afls[1] = new Afiliado("Linares", "Ángel", "Huancayo", 2, 1000);
+        afls[2] = new Afiliado("Lobo", "Víctor", "Cusco", 3, 5000);
+        for (Afiliado a : afls) {
+            System.out.println(a.toString());
+        }
+        System.out.println("-----------------------------------------------------------------------");
     }
 }
 
@@ -73,8 +88,13 @@ class Afiliado extends Person implements Pedido {
 
     @Override
     public String orden() {
-        String msg = " El afiliado " + getId() + " debe vender de : " + getMonto() + " Soles";
+        String msg = " El afiliado " + getId() + " - " + getNombre() + ", debe vender de : " + getMonto() + " Soles";
         return msg;
+    }
+
+    @Override
+    public String toString() {
+        return getId() + ": " + getNombre() + ", cuota -> S/ " + getMonto();
     }
 
 }
@@ -99,6 +119,11 @@ class Cliente extends Person implements Pedido {
     public String orden() {
         String msg = " El cliente " + getApellido() + " " + getNombre() + " telefono: " + getTelefono();
         return msg;
+    }
+
+    @Override
+    public String toString() {
+        return getApellido() + ", " + getNombre() + " Tel: " + getTelefono();
     }
 
 }
